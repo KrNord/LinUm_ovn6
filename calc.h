1dde78c6079b5ed13bdca4e6f949806a6e0bac71
KrNord <kristin_nordstrom@hotmail.com>
2017-04-02 23:15:12 +0200

    Add files via upload

---
+++ calc.h
@@ -0,0 +1,12 @@
+#ifndef __ADD_H__
+#define __ADD_H__
+
+float calc_resistance(int x, char y, float *a);
+
+#endif
+
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+
+
+int main ()
+{
+	int count; 
+	char conn;
+	float resultat;
+
+	printf("Ange koppling [S | P]: ");		
+ 	scanf( " %c", &conn);
+									
+	printf("Antal komponenter: ");
+	scanf( "%d", &count);
+
+	float array[count];
+
+	resultat = calc_resistance(count, conn, array);
+
+	printf("Den resulterande resistansen blir: %.0f\n", resultat);
+
+	exit(0);
+}
+
+
+
+
