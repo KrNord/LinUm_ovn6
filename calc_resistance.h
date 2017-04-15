f42ac7b133e5e6ac03221f58a385802a277c52bf
KrNord <kristin_nordstrom@hotmail.com>
2017-04-15 12:29:30 +0200

    Delete calc_resistance.h

--- calc_resistance.h
+++
@@ -1,8 +0,0 @@
-
-/*
-This is libpower.h. It declares the functions calc_power_r and calc_power_i for users
-*/
-
-float calc_power_r(float volt, float resistance);
-float calc_power_i(float volt, float current);
-	
